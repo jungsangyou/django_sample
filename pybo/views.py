b083995f2a969cb9e29d7d715b3d6c0847b8d1d0
jungsangyou <jungsang.you@sk.com>
2023-03-07 17:59:31 +0900

    2장 완료 3장 시작 전

--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.utils import timezone
 
+from .forms import QuestionForm, AnswerForm
 from .models import Question
+
 
 # Create your views here.
 def index(request):
@@ -49,6 +51,41 @@ def answer_create(request, question_id):
     """
     
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST' : 
+        form = AnswerForm(request.POST) 
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id = question.id)
+    else :
+        form = AnswerForm()
+    
+    context = {'question' : question, 'form' : form}     
+    
+    return render(request, 'pybo/question_detail.html', context)
+                  
+def question_create(request):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+    """
+    
+    if request.method == 'POST' :
+        form = QuestionForm(request.POST)
+        if form.is_valid() :
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    
+    else : 
+        form  = QuestionForm()
+    context = {'form' : form}
+    
+    return render(request, 'pybo/question_form.html', context)    
+
+
